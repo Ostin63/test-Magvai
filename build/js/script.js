4af6aaee2093dab5c9681fc59cb42d19fe081cc1
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-07 21:02:46 +0400

    https замена

--- build/js/script.js
+++ build/js/script.js
@@ -7,12 +7,9 @@ let button = document.querySelector('.button');
 button.addEventListener('click', function () {
   textContainer.classList.remove('d-none');
   async function main() {
-
     clicks += 6;
-
-    let response = await fetch('http://jsonplaceholder.typicode.com/posts?_start=' + clicks + '&_limit=6');
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks + '&_limit=6');
     let arr = await response.json();
-
     arr.forEach(function (element) {
       let templateClone = template.cloneNode(true);
       templateClone.querySelector('.title').textContent +=
