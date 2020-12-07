f368f78d0fd6a77ebb392e74296bd4c3164e5425
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-07 20:44:16 +0400

    добавление счетчика в url

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-
 let textContainer = document.querySelector('.content');
 let template = document.querySelector('#title-template').content;
 
 // template.
-
+let clicks = 0;
 let button = document.querySelector('.button');
 button.addEventListener('click', function () {
   textContainer.classList.remove('d-none');
   async function main() {
-
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    
+    clicks += 6;
+    
+    let response = await fetch('http://jsonplaceholder.typicode.com/posts?_start=' + clicks +'&_limit=6');
     let arr = await response.json();
-    arr.forEach(function (element) {
 
+    arr.forEach(function (element) {
       let templateClone = template.cloneNode(true);
-      templateClone.querySelector('.title > span').textContent +=
+      templateClone.querySelector('.title').textContent +=
         ' ' + element.title;
-      templateClone.querySelector('.body > span').textContent +=
+      templateClone.querySelector('.body').textContent +=
         ' ' + element.body;
       textContainer.appendChild(templateClone);
     });
