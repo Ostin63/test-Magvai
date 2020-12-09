75f29bf13a1b7efb3df223d3d2b00ade2a35ef4a
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-09 19:27:19 +0400

    убрал функцию

--- js/script.js
+++ js/script.js
@@ -2,25 +2,23 @@ let textContainer = document.querySelector('.content');
 let template = document.querySelector('#title-template').content;
 
 // template.
+async function main() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks + '&_limit=6');
+  let arr = await response.json();
+  clicks += 6;
+  arr.forEach(function (element) {
+    let templateClone = template.cloneNode(true);
+    templateClone.querySelector('.title').textContent +=
+      ' ' + element.title;
+    templateClone.querySelector('.body').textContent +=
+      ' ' + element.body;
+    textContainer.appendChild(templateClone);
+  });
+}
+main();
 let clicks = 0;
 let button = document.querySelector('.button');
 button.addEventListener('click', function () {
   textContainer.classList.remove('d-none');
-  async function main() {
-    
-    
-    
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks +'&_limit=6');
-    let arr = await response.json();
-    clicks += 6;
-    arr.forEach(function (element) {
-      let templateClone = template.cloneNode(true);
-      templateClone.querySelector('.title').textContent +=
-        ' ' + element.title;
-      templateClone.querySelector('.body').textContent +=
-        ' ' + element.body;
-      textContainer.appendChild(templateClone);
-    });
-  }
   main();
 });
