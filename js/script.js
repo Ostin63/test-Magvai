04a86e51453a85144870b6911806ed1829fb8113
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-09 19:32:17 +0400

    убрал main

--- js/script.js
+++ js/script.js
@@ -3,20 +3,21 @@ let template = document.querySelector('#title-template').content;
 
 // template.
 async function main() {
-  let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks + '&_limit=6');
-  let arr = await response.json();
-  clicks += 6;
-  arr.forEach(function (element) {
-    let templateClone = template.cloneNode(true);
-    templateClone.querySelector('.title').textContent +=
-      ' ' + element.title;
-    templateClone.querySelector('.body').textContent +=
-      ' ' + element.body;
-    textContainer.appendChild(templateClone);
-  });
-}
-main();
-let clicks = 0;
+  let clicks = 0;
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks +'&_limit=6');
+    let arr = await response.json();
+    clicks += 6;
+    arr.forEach(function (element) {
+      let templateClone = template.cloneNode(true);
+      templateClone.querySelector('.title').textContent +=
+        ' ' + element.title;
+      templateClone.querySelector('.body').textContent +=
+        ' ' + element.body;
+      textContainer.appendChild(templateClone);
+    });
+  }
+  main();
+
 let button = document.querySelector('.button');
 button.addEventListener('click', function () {
   textContainer.classList.remove('d-none');
