3279772892464b194f7fe81f709dd5b5cbfdab5f
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-09 19:05:09 +0400

    исправление счетчика

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ let textContainer = document.querySelector('.content');
 let template = document.querySelector('#title-template').content;
 
 // template.
-let clicks = -6;
+let clicks = 0;
 let button = document.querySelector('.button');
 button.addEventListener('click', function () {
   textContainer.classList.remove('d-none');
   async function main() {
     
-    clicks += 6;
+    
     
     let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks +'&_limit=6');
     let arr = await response.json();
-
+    clicks += 6;
     arr.forEach(function (element) {
       let templateClone = template.cloneNode(true);
       templateClone.querySelector('.title').textContent +=
