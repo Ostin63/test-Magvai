c1d4eb160b2de0a94d23224d716639fb5b5c007b
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-09 19:58:41 +0400

    async

--- js/script.js
+++ js/script.js
@@ -2,24 +2,20 @@ let textContainer = document.querySelector('.content');
 let template = document.querySelector('#title-template').content;
 
 // template.
-async function main() {
-  let clicks = 0;
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks +'&_limit=6');
-    let arr = await response.json();
-    clicks += 6;
-    arr.forEach(function (element) {
-      let templateClone = template.cloneNode(true);
-      templateClone.querySelector('.title').textContent +=
-        ' ' + element.title;
-      templateClone.querySelector('.body').textContent +=
-        ' ' + element.body;
-      textContainer.appendChild(templateClone);
-    });
-  }
-  main();
 
 let button = document.querySelector('.button');
-button.addEventListener('click', function () {
+button.addEventListener('click', async function () {
   textContainer.classList.remove('d-none');
-  main();
+  let clicks = 0;
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=' + clicks + '&_limit=6');
+  let arr = await response.json();
+  clicks += 6;
+  arr.forEach(function (element) {
+    let templateClone = template.cloneNode(true);
+    templateClone.querySelector('.title').textContent +=
+      ' ' + element.title;
+    templateClone.querySelector('.body').textContent +=
+      ' ' + element.body;
+    textContainer.appendChild(templateClone);
+  });
 });
